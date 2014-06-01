f1a63f9cd7ca43358fd82c91190cf7ede9693743
Katherine Fellows <k@kpf.me>
2014-06-01 13:55:00 -0700

    added unit tests for average

--- test/simple_clojure_store/test/handler.clj
+++ test/simple_clojure_store/test/handler.clj
@@ -2,4 +2,12 @@
   (:use clojure.test
         simple-clojure-store.handler))
 
+(deftest test-average
+  (testing "average"
+    (is (= 11/2 (average [1 2 3 4 5 6 7 8 9 10]))))
 
+  (testing "average of empty collection"
+    (is (= nil (average []))))
+
+  (testing "average of collection with size 1"
+    (is (= 1 (average [1])))))
