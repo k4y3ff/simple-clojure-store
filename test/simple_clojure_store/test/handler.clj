7df2086d45945f498517fb899a4536067cdad397
Katherine Fellows <k@kpf.me>
2014-06-01 13:57:01 -0700

    added unit tests for average-of-averages

--- test/simple_clojure_store/test/handler.clj
+++ test/simple_clojure_store/test/handler.clj
@@ -20,3 +20,18 @@
   (testing "average for key contained in map"
     (let [kv-map {:key [1 2 3]}]
       (is (= 2 (average-for-key kv-map :key))))))
+
+(deftest test-average-of-averages
+  (testing "average of averages for empty map"
+    (let [kv-map {}]
+      (is (= nil (average-of-averages kv-map)))))
+
+  (testing "average of averages for map with one key"
+    (let [kv-map {:key1 [1 2 3]}]
+      (is (= 2 (average-of-averages kv-map)))))
+
+  (testing "average of averages for map with multiple keys"
+    (let [kv-map {:key1 [1 2 3]
+                  :key2 [4 5 6]
+                  :key3 [7 8 9]}]
+      (is (= 5 (average-of-averages kv-map))))))
